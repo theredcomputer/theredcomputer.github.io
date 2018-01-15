fd59d09b3bd1f510e64ee902d7a3ed8d19e004b6
Stephen Eick <prinnydude97@gmail.com>
2018-01-14 23:41:09 -0500

    Began creation of new site.

---
+++ tests/menu.js
@@ -0,0 +1,3 @@
+var element = '<ul class="list-unstyled"><li class="nav-li"><a href="#">Bio</a></li><li class="nav-li"><a href="#">Publications</a></li><li class="nav-li"><a href="#">CV</a></li><li class="nav-li"><a href="#">Photography</a></li><li class="nav-li"><a href="#">Miscellanea</a></li><li class="nav-li"><a href="contact.html">Contact Me</a></li></ul>';
+
+var footer_text = '<p><i>"Truth is always exciting. Speak it, then. Life is dull without it." &mdash; Pearl S. Buck</i></p><p>Website designed by Stephen Eick.</p>';
