6a7a6b1923644a29694f119448d85e108d6e1d54
Stephen Eick <prinnydude97@gmail.com>
2018-01-16 12:47:42 -0500

    Modified styling and edited content.

--- tests/menu.js
+++ tests/menu.js
@@ -1,3 +1,3 @@
 var element = '<ul class="list-unstyled"><li class="nav-li"><a href="index.html">Home</a></li><li class="nav-li"><a href="bio.html">Bio</a></li><li class="nav-li"><a href="publications.html">Publications</a></li><li class="nav-li"><a href="cv.pdf">CV</a></li><li class="nav-li"><a href="photography.html">Photography</a></li><li class="nav-li"><a href="miscellanea.html">Miscellanea</a></li><li class="nav-li"><a href="contact.html">Contact Me</a></li></ul>';
 
-var footer_text = '<p><i>"Truth is always exciting. Speak it, then. Life is dull without it." &mdash; Pearl S. Buck</i></p><p>Website designed by Stephen Eick.</p>';
+var footer_text = '<p><i>"Truth is always exciting. Speak it, then. Life is dull without it." &mdash; Pearl S. Buck</i></p><p>Website designed in 2018 by Stephen Eick.</p><p><a href="https://linkedin.com/in/stepheneick"><i class="fab fa-linkedin fa-lg"></i></a> </p>';
