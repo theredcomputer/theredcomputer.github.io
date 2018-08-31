a645106e6101e8d1a60fb893a6657efaa8ada422
Stephen Eick <prinnydude97@gmail.com>
2018-08-31 00:13:09 -0400

    New background, more tweaks

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-var element = '<ul class="list-unstyled"><li class="nav-li"><a href="index.html">Home</a></li><li class="nav-li"><a href="cv.pdf">CV/Resume</a></li><li class="nav-li"><a href="publications.html">Publications</a></li><li class="nav-li"><a href="photography.html">Photography</a></li><li class="nav-li"><a href="personal.html">Personal</a></li><li class="nav-li"><a href="contact.html">Contact Me</a></li></ul>';
+var element = '<ul class="list-unstyled nav-ul"><li class="nav-li"><a href="index.html">Home</a></li><li class="nav-li"><a href="cv.pdf">CV/Resume</a></li><li class="nav-li"><a href="publications.html">Publications</a></li><li class="nav-li"><a href="photography.html">Photography</a></li><li class="nav-li"><a href="personal.html">Personal</a></li><li class="nav-li"><a href="contact.html">Contact Me</a></li></ul>';
 
 var footer_text = '<p><i>"Truth is always exciting. Speak it, then. Life is dull without it." &mdash; Pearl S. Buck</i></p><p>Website designed in 2018 by Stephen Eick.</p><p><a target="_blank" href="https://linkedin.com/in/stepheneick"><i class="fab fa-linkedin fa-lg"></i></a> </p>';
 
